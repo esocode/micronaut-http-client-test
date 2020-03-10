9fa32852f4d156902ac3246f0a28e46b15f3041f
Elmar Sonnenschein <Elmar.Sonnenschein.ipsways@md.de>
2020-03-10 12:36:49 +0100

    Service interface moved to lib

--- src/main/java/io/micronaut/test/ServiceClient.java
+++ src/main/java/io/micronaut/test/ServiceClient.java
@@ -1,7 +1,13 @@
 package io.micronaut.test;
 
+import io.micronaut.http.annotation.PathVariable;
 import io.micronaut.http.client.annotation.Client;
+import io.reactivex.Maybe;
 
 @Client("service")
 public interface ServiceClient extends Service {
+
+	// will only compile if method is duplicated
+	//	@Override
+	//	Maybe<Integer> getIdForName(@PathVariable String forName);
 }
